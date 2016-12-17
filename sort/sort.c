2a02fa53e4d717b202575fd6c36d917e7bc0df7b
Manato <manato0x2cc@gmail.com>
2016-12-17 13:45:57 +0900

    Create sort (what is name?)
    There is way faster than this  isn't there?

--- sort/sort.c
+++ sort/sort.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
 
+void sort1(void);
+
+/* テスト用のデータ */
+int numbers[9] = {7,4,5,3,8,9,3,7,2};
+
+/* テストデータ配列の長さ
+ * マクロにしたほうがいいかな
+ */
+int length = 9;
+
+int main(void) {
+  sort1();
+  for (size_t i = 0; i < length; i++) {
+    printf("%d\n", numbers[i]);
+  }
+  return 0;
+}
+
+void sort1(void){
+  int keep,s;
+  for (size_t i = length; i > 0; i--) {
+    s = length;
+    while (s >= 1) {
+      if(numbers[s] > numbers[s-1]) s--;
+      else {
+        keep = numbers[s];
+        numbers[s] = numbers[s-1];
+        numbers[--s] = keep;
+      }
+    }
+  }
+}
